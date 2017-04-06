79c38616fb40872c4f3b9d647c863c187e28902e
Luis <josergp94@gmail.com>
2017-04-06 15:58:22 -0300

    Defino scrapper para animeyt

---
+++ scrappers.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import time
+from bs4 import BeautifulSoup as Bs
+import cfscrape as cfs
+import externos.pynet.pynet as net
+import externos.pynet.modules.colors as clrs
+
+default_download_path = 'descargas'
+
+class Scraper():
+    cache = {}
+    sesion = None
+    base_link = ''
+    scraper_name = ''
+    dl_path = ''
+
+    def __init__(self, base_path):
+        self.dl_path = base_path + '/'
+        self.dl_path += self.scraper_name + '/'
+        self.sesion = net.NetHandler(cfs.create_scraper())
+        self.preparar_antibot_cf()
+
+    def preparar_antibot_cf(self):
+        clrs.m_aviso('Preparando contra CloudFlare...')
+        inicial = time.clock()
+        hreq = self.sesion.request_get(self.base_link)
+        final = time.clock()
+        clrs.m_aviso("Se tardó %s segundos" % str(round((final-inicial)*10000)))
+        if (hreq.status_code==200):
+            clrs.m_aviso('Completado')
+        else:
+            m_aviso('ocurrio un problema')
+
+    def buscar_en_cache(self, criterio):
+        return self.cache.get(criterio)
+
+    def buscar(self, criterio):
+        resultados = self.buscar_en_cache(criterio)
+        if (resultados is None):
+            url = self.url_busqueda(criterio)
+            resultados = self.get_results_from_url(url)
+            self.cache.update({criterio:resultados})
+        return resultados
+
+    def descargar(self, serie, desde=None):
+        self.create_if_not_exists(self.dl_path.split('/')[0])
+        self.create_if_not_exists(self.dl_path)
+        serie_path = self.dl_path + serie['title'] + '/'
+        self.create_if_not_exists(serie_path)
+        capitulos_totales = self.get_capitulos_from_url(serie['link'])
+        if (desde is None):
+            capitulos = capitulos_totales
+        else:
+            if (desde >= len(capitulos)):
+                clrs.m_aviso("seleccion no disponible")
+            capitulos =  capitulos_totales[desde-1:]
+        for capitulo in capitulos:
+            self.descargar_capitulo(capitulo, serie_path)
+
+    def descargar_capitulo(self, capitulo, dir_path):
+        path_capitulo = dir_path + capitulo['title'] + 'mp4'
+        if os.path.exists(path_capitulo):
+            clrs.m_aviso("Ya existe %s" % path_capitulo)
+        else:
+            cap_dl_link = self.get_download_link_from_url(capitulo['link'])
+            try:
+                clrs.m_aviso("using native wget")
+                status = os.system('wget -O "%s" "%s"' % (path_capitulo, cap_dl_link))
+                if (status != 0):
+                    print status
+                    clrs.m_interr('\nCancelado wget')
+                    os.system('rm "%s"' % path_capitulo)
+                if (status == 2048):
+                    clrs.m_aviso("Wget failed")
+                    0/0
+            except Exception as e:
+                clrs.m_aviso("Wget failed")
+                clrs.m_aviso("Trying requests package mode")
+                self.sesion.download_file(cap_dl_link, path_capitulo)
+
+    def create_if_not_exists(self, dir_path):
+        if os.path.exists(dir_path):
+            clrs.m_aviso("Ya existe %s" % dir_path)
+        else:
+            clrs.m_aviso("No existe %s" % dir_path)
+            os.mkdir(dir_path)
+
+class AnimeytScraper(Scraper):
+    base_link = 'http://www.animeyt.tv/'
+    scraper_name = 'animeyt'
+    re_link_js = re.compile('.*url = \"(.*?)\";.*')
+
+    ##metodos de busqueda
+    def url_busqueda(self, criterio):
+        busqueda_prev = '%sbusqueda' % self.base_link
+        return '%s?terminos=%s' % (busqueda_prev, criterio.replace(' ', '+'))
+
+    def get_results_from_url(self, url):
+        sreq = self.sesion.request_get(url)
+        s_text = Bs(sreq.text, 'lxml')
+        l_anime_divs = s_text.findAll('article', {'class': 'anime'})
+        l_d_animes = map(self.get_anime_from_div, l_anime_divs)
+        return l_d_animes
+
+    def get_anime_from_div(self, anime_div):
+        d_anime = {}
+        d_anime.update({'title': self.get_title(anime_div)})
+        d_anime.update({'poster': self.get_poster_link(anime_div)})
+        d_anime.update({'synopsis': self.get_synopsis(anime_div)})
+        d_anime.update({'link': self.get_link(anime_div)})
+        d_anime.update({'date': self.get_date(anime_div)})
+        d_anime.update({'status': self.get_status(anime_div)})
+        d_anime.update({'genres': self.get_genres(anime_div)})
+        d_anime.update({'tags': self.get_tags(anime_div)})
+        return d_anime
+
+    def get_poster_link(self, anime_div):
+        poster_link = anime_div.find('img', {'class': 'anime__img'}).get('src')
+        return poster_link
+
+    def get_title(self, anime_div):
+        anime_title = anime_div.find('h3', {'class': 'anime__title'}).getText()
+        return anime_title
+
+    def get_synopsis(self, anime_div):
+        anime_synopsis = anime_div.find(
+            'p', {'class': 'anime__synopsis js-synopsis-reduce'}).getText()
+        return anime_synopsis
+
+    def get_link(self, anime_div):
+        anime_link = anime_div.find('a', {'anime__synopsis-container'}).get('href')
+        return anime_link
+
+    def get_date(self, anime_div):
+        anime_date = anime_div.find('span', {'class': 'icon-fecha'}).getText()
+        return anime_date
+
+    def get_status(self, anime_div):
+        anime_status = anime_div.find('span', {'class': 'anime__status'}).getText()
+        return anime_status
+
+    def get_genres(self, anime_div):
+        l_span_genre = anime_div.findAll('span', {'class': 'anime__genre'})
+        l_genres = map(self.get_genre, l_span_genre)
+        return l_genres
+
+    def get_genre(self, genre_span):
+        genre = genre_span.getText()
+        return genre
+
+    def get_tags(self, anime_div):
+        l_span_tag = anime_div.findAll('span', {'class': 'anime__tag'})
+        l_tags = map(self.get_tag, l_span_tag)
+        return l_tags
+
+    def get_tag(self, tag_span):
+        tag = tag_span.getText()
+        return tag
+
+    #scrapping de obtencion de capitulos
+    def get_capitulos_from_url(self, url_serie):
+        creq = self.sesion.request_get(url_serie)
+        c_text = Bs(creq.text, 'lxml')
+        l_chapters = c_text.findAll(
+        'div', {'class': 'serie-capitulos__list__item'})
+        l_d_chapters = map(self.get_chapter_from_div, l_chapters)
+        l_d_chapters.reverse()
+        return l_d_chapters
+
+    def get_chapter_from_div(self, chapter_div):
+        d_chapter = {}
+        d_chapter.update({'title': self.get_chapter_title(chapter_div)})
+        d_chapter.update({'link': self.get_chapter_link(chapter_div)})
+        return d_chapter
+
+
+    def get_chapter_title(self, chapter_div):
+        chapter_title = chapter_div.find('a').getText().replace('\n', ' ')
+        return chapter_title
+
+
+    def get_chapter_link(self, chapter_div):
+        chapter_link = chapter_div.find('a').get('href')
+        return chapter_link
+
+    #scrapping de obtencion de links de descarga de capitulo
+    def get_download_link_from_url(self, url_capitulo):
+        c_req = self.sesion.request_get(url_capitulo)
+        c_text = Bs(c_req.text, 'lxml')
+        download_url_redir = c_text.find(
+            'a', {'target': '_blank'}, text='Descarga').get('href')
+        ## ubico enlace generado de descarga
+        download_url = self.get_link_by_link_page(download_url_redir)
+        return download_url
+
+    def get_link_by_link_page(self, dl_link):
+        b_text = Bs(self.sesion.request_get(dl_link).text, 'lxml')
+        script_tag = b_text.findAll('script')[1]
+        text_url = script_tag.string.split('\n')[8]
+        url_real = self.re_link_js.match(text_url).groups()[0]
+        return url_real
+
+
+def get_animeyt_scrapper():
+    return AnimeytScraper(default_download_path)
